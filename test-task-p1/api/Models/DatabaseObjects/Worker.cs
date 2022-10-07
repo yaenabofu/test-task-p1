ea7e29cd119bcfd326ec4b96fbe2f45691f0f99f
AntonBazhutin <nezzterlivejuke@gmail.com>
2022-10-07 20:25:27 +0500

    Add database models

---
+++ test-task-p1/api/Models/DatabaseObjects/Worker.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace api.Models.DatabaseObjects
+{
+    public class Worker
+    {
+        public int Id { get; set; }
+        public string Surname { get; set; }
+        public string Name { get; set; }
+        public string ThirdName { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Snils { get; set; }
+        public int CompanyId { get; set; }
+    }
+}
